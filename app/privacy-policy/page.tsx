794d45f630fa80a3fb5de5be9c734a9a99598157
Matteo <matteo@meanme.com>
2023-05-06 13:49:49 -0700

    privacy policy

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,95 @@
+export default function PrivacyPolicy() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <code className="font-mono font-bold">privacy-policy</code>
+        </p>
+      </div>
+
+      <div className="w-full ">
+        <div className="mt-8 text-2xl font-large">Privacy Policy for our Games</div>
+
+        This Privacy Policy governs the manner in which meanme.com ("we", "us", "our") collects, uses, maintains and discloses information collected from users (each, a "User") of our titles ("Game") and related services.
+
+        <div className="mt-8 text-xl font-medium">Personal Information Collection</div>
+        We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users play our Game, register on our Game, and in connection with other activities, services, features or resources we make available on our Game. Users may be asked for, as appropriate, name, email address, and other relevant information. We will collect personal identification information from Users only if they voluntarily submit such information to us.
+
+        <div className="mt-8 text-xl font-medium">
+        Non-Personal Information Collection
+        </div>
+        We may collect non-personal identification information about Users whenever they interact with our Game. Non-personal identification information may include the device type, device identifier, and other technical information about Users' means of connection to our Game, such as the operating system and the Internet service providers utilized and other similar information.
+        
+        <div className="mt-8 text-xl font-medium">
+        Unity Analytics Integration
+        </div>
+        Our Game uses Unity Analytics, which collects certain non-personal information automatically. Unity Analytics provides us with analytics and other data to help us understand how Users interact with our Game. Unity Analytics collects and uses data in accordance with Unity Technologies’ privacy policy.
+
+        <div className="mt-8 text-xl font-medium">
+        Third-Party Services
+        </div>
+
+        We may use third-party services to help us operate our Game and the services we offer. These third-party services may have access to certain personal and non-personal information collected through our Game. We do not control how these third-party services use such information and we encourage Users to review the privacy policies of these third-party services before using them.
+
+        <div className="mt-8 text-xl font-medium">
+        Use of Information
+        </div>
+
+        We may use personal and non-personal information collected from Users for the following purposes:
+
+        <ul>
+
+        <li>To personalize User experience</li>
+        <li>To improve our Game and related services</li>
+        <li>To respond to User requests and support needs</li>
+        <li>To provide Users with promotional and marketing materials</li>
+        <li>To comply with legal obligations</li>
+        <li>Sharing of Information</li>
+        </ul>
+
+        <br/>
+
+        We may share personal and non-personal information collected from Users with the following third-party service providers:
+
+        <div className="mt-2 text-l font-medium">
+        Unity Analytics
+        </div>
+        Third-party services used to operate our Game and related services
+        We may also share information if we believe it is necessary to comply with a legal obligation or to protect our rights, property, or safety or the rights, property, or safety of others.
+
+
+        <div className="mt-8 text-xl font-medium">
+        Security
+        </div>
+
+We take reasonable measures to help protect personal and non-personal information collected from Users from unauthorized access, use, disclosure, alteration or destruction. However, no method of transmission over the Internet or method of electronic storage is 100% secure.
+
+
+<div className="mt-8 text-xl font-medium">
+  Changes to this Privacy Policy
+        </div>
+
+We have the discretion to update this Privacy Policy at any time. We encourage Users to frequently check this page for any changes. You acknowledge and agree that it is your responsibility to review this Privacy Policy periodically and become aware of modifications.
+
+<div className="mt-8 text-xl font-medium">
+Your Acceptance of these Terms
+        </div>
+By using our Game, you signify your acceptance of this Privacy Policy. If you do not agree to this Privacy Policy, please do not use our Game. Your continued use of our Game following the posting of changes to this Privacy Policy will be deemed your acceptance of those changes.
+
+<div className="mt-8 text-xl font-medium">
+Contacting Us
+</div>
+
+
+If you have any questions about this Privacy Policy, please contact us at matteo@meanme.com.
+
+      </div>
+      <div>
+
+      </div>
+
+      
+      
+    </main>
+  )
+}
