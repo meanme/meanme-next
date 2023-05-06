af518b7f8619ae78b46d90dc51cf150283929a0e
Matteo <matteo@meanme.com>
2023-05-06 13:17:25 -0700

    Force update of gh-pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <code className="font-mono font-bold">meanme.com</code>
         </p>
-        
       </div>
       
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-       
       </div>
     </main>
   )
